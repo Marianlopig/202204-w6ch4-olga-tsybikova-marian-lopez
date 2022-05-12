7aa35a7edfc5b3c82e282849f5a09a28537a5153
Marian <marianlopig@gmail.com>
2022-05-12 20:27:17 +0200

    Added the main array with a list of 5 things we already know

---
+++ thingsIKnow.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const thingsIKnow = [
+  {
+    id: 1,
+    thing: "frontend",
+  },
+  {
+    id: 2,
+    thing: "testing",
+  },
+  {
+    id: 3,
+    thing: "Redux-Flux",
+  },
+  {
+    id: 4,
+    thing: "Classes",
+  },
+  {
+    id: 5,
+    thing: "Sass",
+  },
+];
+
+module.exports = thingsIKnow;
